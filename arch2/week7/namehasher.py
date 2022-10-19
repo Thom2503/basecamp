411294f167460c3ce167d6a85dcc916e71bb46f9
Thom2503 <thomveldhuis03@gmail.com>
2022-10-19 19:15:12 +0200

    [arch:2] Assignment van week 7 af ingeleverd en alles

--- arch2/week7/namehasher.py
+++ arch2/week7/namehasher.py
@@ -42,12 +42,14 @@ a_b?c9d6e1f4g!h:i<j|k{l0m@n7o+p~q2r+s/t=u^v3w]x(y-z>A*B8C;D%E#F}G5H)I[J$
 To test your functions, use the provided unit test file (for boilerplate) and complete the test functions with your own code.
 '''
 
+import sys
+
 hashmap_key_value = {}
 encoded_values = []
 decoded_values = []
 
 
-def search_letter(code):
+def search_letter(code: str) -> str:
     """
     Vind de key van met de code als value
 
@@ -63,8 +65,11 @@ def search_letter(code):
 # create a function that given the input string converts it to the encoded equivalent based on the provided or already set key/hashmap
 # make sure to only convert values that are in the key/hashmap, if the value is not present, use its own value
 def encode_string(data: str, key: str = None) -> str:
+    # de uiteindelijk geencode string
     encoded = ""
-    
+    # loop door de string heen om de characters in
+    # de hash map over te zetten om het te encrypten
+    # in het voorbeeld heeft elke letter een speciaal teken
     for char in data:
         if char in hashmap_key_value:
             encoded += hashmap_key_value[char]
@@ -77,8 +82,11 @@ def encode_string(data: str, key: str = None) -> str:
 # create a function that given the input string converts it to the decoded equivalent based on the provided or already set key/hashmap
 # make sure to only decode values that are in the key/hashmap, if the value is not present, use its own value
 def decode_string(data: str, key: str = None) -> str:
+    # de uiteindelijk gedecode string
     decoded = ""
-
+    # loop door de string heen en als er een value
+    # is gevonden is moet het gedecrypt worden naar
+    # de originele waarde
     for char in data:
         if char in hashmap_key_value.values():
             decoded += search_letter(char)
@@ -93,13 +101,9 @@ def decode_string(data: str, key: str = None) -> str:
 # tip! make use of the map function within python with a lambda to call the internal function with all elements
 # as a return value, you should return a list with Tuples containing the decoded value as first value and the encode value as second value
 def encode_list(data: list, key: str = None) -> list:
-    encoded_list = []
-
-    for to_encode in data:
-        encoded = encode_string(to_encode)
-        encoded_list.append(encoded)
-
-    return encoded_list
+    # map over de lijst heen en encode het dan allemaal
+    # *lambda kan weg maar moet helaas van codegrade :P*
+    return list(map(lambda data_str: encode_string(data_str), data))
 
 
 # create a function that given a list of inputs converts the complete list to the encoded equivalent based on the key/hashmap
@@ -107,19 +111,17 @@ def encode_list(data: list, key: str = None) -> list:
 # tip! make use of the map function within python with a lambda to call the internal function with all elements
 # as a return value, you should return a list with Tuples containing the decoded value as first value and the encode value as second value
 def decode_list(data: list, key: str = None) -> list:
-    decoded_list = []
-
-    for to_decode in data:
-        decoded = decode_string(to_decode)
-        decoded_list.append(decoded)
-
-    return decoded_list
+    # map over de lijst heen en decode alles
+    return list(map(decode_string, data))
 
 
 # create a function that given a encoded value, decoded value and a key/hashmap (optional) checks if the values are correct
 # the return value should be a boolean value (True if values match, False if they don't match)
 def validate_values(encoded: str, decoded: str, key: str = None) -> bool:
-    ...
+    # vergelijk of de encoded string hetzelfde is als de
+    # decoded string door de encoded string te decoden
+    # zo hoef je alleen met 1 iets te doen
+    return decode_string(encoded, key) == decoded
 
 
 # give the option to input a hashvalue to be used/converted to a key/hashmap
@@ -128,18 +130,12 @@ def validate_values(encoded: str, decoded: str, key: str = None) -> bool:
 # example: a@b.c>d#eA will become: {'a': '@', 'b': '.', 'c': '>', 'd', '#', 'e': 'A'}
 def set_hashmap(key: str) -> None:
     global hashmap_key_value
-    for i in range(0, len(key), 2):
-        dict_key = key[i]
-        dict_val = key[i + 1]
-        hashmap_key_value.update({dict_key: dict_val})
+    # maak met zip 2 lijstjes (dit zijn de even en oneven karakters)
+    # die met dict een dictionary worden
+    hashmap_key_value = dict(zip(key[::2], key[1::2]))
 
 
 
-set_hashmap("A%B&C(D)E*F+G-H/I0J<K=L1M!N9O?P>Q7R#S5T;U:V[W]X~Y$Z@")
-print(encode_string("EEN CORRECTE UITKOMST"))
-print(decode_string("%9)*#5?! ]*#=; ??="))
-print(encode_list(["PIETER", "PAN"]))
-print(decode_list([">0*;*#", ">%9"]))
 # build menu structure as following
 # the input can be case-insensitive (so E and e are valid inputs)
 # [E] Encode value to hashed value
@@ -148,7 +144,60 @@ print(decode_list([">0*;*#", ">%9"]))
 # [V] Validate 2 values against eachother
 # [Q] Quit program
 def main():
-    ...
+    # zet de key om mee te hashen
+    print("""[E] Encode value to hashed value
+[D] Decode hashed value to normal value
+[P] Print all encoded/decoded values
+[V] Validate 2 values against eachother
+[Q] Quit program""")
+    user_key = input("Input a key to use:\n")
+    # check of de key wel even is
+    if len(user_key) % 2 != 0:
+        print("Invalid hashvalue input")
+        sys.exit()
+    # als de key even is kan er een hash map van gemaakt worden
+    set_hashmap(user_key)
+    # vraag de gebruiker voor een commando
+    command = input().lower()
+    if command == "q":
+        # als je q invult moet je het programma stoppen
+        sys.exit()
+    elif command == "e":
+        value_to_encode = input("Input a value to encode:\n")
+        # als er een comma in de input staat moeten er meerdere
+        # worden geencode worden dus maak daar een lijstje van en
+        # voeg die aan de lijst toe
+        if "," in value_to_encode:
+            values_to_encode = value_to_encode.replace(", ", ",").split(",")
+            decoded_values.extend(values_to_encode)
+        else:
+            decoded_values.append(value_to_encode)
+    elif command == "d":
+        value_to_decode = input("Enter a value to decode:\n")
+        if "," in value_to_decode:
+            values_to_decode = value_to_decode.replace(", ", ",").split(",")
+            encoded_values.extend(values_to_decode)
+        else:
+            encoded_values.append(value_to_decode)
+    elif command == "v":
+        encode_to_compare = input("Give encoding to compare:\n")
+        decode_to_compare = input("Give decoding to compare:\n")
+        print(validate_values(encode_to_compare, decode_to_compare))
+        sys.exit()
+    else:
+        sys.exit()
+    # vraag een tweede keer voor input om het bijvoorbeeld uit te printen
+    command = input().lower()
+    if command == "p":
+        # print alles uit als er values zijn om te printen
+        # zet ze netjes onder elkaar bij het uitprinten
+        if len(encoded_values) > 0:
+            print("\n".join(decode_list(encoded_values)))
+        if len(decoded_values) > 0:
+            print("\n".join(encode_list(decoded_values)))
+    else:
+        sys.exit()
+
 
 
 # Create a unittest for both the encode and decode function (see test_namehasher.py file for boilerplate)
